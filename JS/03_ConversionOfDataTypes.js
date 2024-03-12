a5242894a16e1c635845d46c7ed252c0cd9b6679
Avinash Marbhal <100125775+avinashmarbhal@users.noreply.github.com>
2024-03-13 00:26:31 +0530

    03_ConversionOfDataTypes_operations

--- JS/03_ConversionOfDataTypes.js
+++ JS/03_ConversionOfDataTypes.js
@@ -4,7 +4,8 @@ let score = "33"//if we convert this string into number then it will be converte
 //let score = "33aa"// if we convert this string into number then it will be converted to NaN(Not a number)
 //let score = "Avinash"//if we convert this string into number then it will be converted to NaN(Not a number)
 //let score = null //if we convert this  into number then it will be converted to 0
-//let score = undefined // if we convert this  into number then it will be converted to NaN(Not a number)
+//let score = undefined // if we convert this  into numb
+er then it will be converted to NaN(Not a number)
 //let score = true // if we convert this  into number then it will be converted to 1 and for false it would be 0
 
 //console.log(typeof score);
@@ -37,4 +38,39 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************* Operations *****************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);//2 rest to 3
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " Avinash"
+let str3 = str1+str2
+//console.log(str3);
+
+
+// console.log("1"+2);// => 12
+// console.log(1+"2");// => 12
+// console.log("1"+2+"2");// => 122
+// console.log(1+2+"2");// => 32
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
