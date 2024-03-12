365a4814a5f91b3fde26916ef3631e5f3a315cc5
Avinash Marbhal <100125775+avinashmarbhal@users.noreply.github.com>
2024-03-12 17:45:58 +0000

    dataTypes

---
+++ JS/dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // if we use this then our all js code is treat as newer version
+
+// alert(3+3) we are using node js not browser so the syntax is different
+
+let name = "Avinash"
+let age = 22
+let isLoggedIn = false
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value is yet to be assign 
+//symbol => unique
+
+// '' Object ''
+
+console.log(typeof undefined); // type of this is Undefined 
+console.log(typeof null); // type of this is Object
