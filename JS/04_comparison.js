50eed64fd20f6323302464cda267d85a78ebe159
Avinash Marbhal <100125775+avinashmarbhal@users.noreply.github.com>
2024-03-13 08:09:18 +0000

    comparison

---
+++ JS/04_comparison.js
@@ -0,0 +1,38 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+
+
+
+
+//in this type of comparison the value/variable is cheked and if the dataTypes of the values are diffrent then the dataType is get chaged 
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+
+
+//while comparison with null/undefined the value of both is converted according to the comaprison operator so, aovide this type of comparisons
+
+// console.log(null > 0); => false
+// console.log(null == 0); => false
+// console.log(null >= 0); => true
+
+// console.log(undefined == 0); =>false
+// console.log(undefined > 0); =>false
+// console.log(undefined < 0);=>false
+
+
+
+
+
+// ===  
+//in this (===) the the datatype of the value/variable is also check and if any one of it's not the same then it will give you a false result 
+
+console.log("2" === 2);
