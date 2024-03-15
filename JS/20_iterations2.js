50f83dfb884596e99611be43e2ef53fd6dbb5225
Avinash Marbhal <100125775+avinashmarbhal@users.noreply.github.com>
2024-03-15 07:15:57 +0000

    while, do_While

---
+++ JS/20_iterations2.js
@@ -0,0 +1,33 @@
+
+// #### while loop #####
+
+let index = 0
+
+// while(index <= 10)
+// {
+//     console.log(`value: ${index}`);
+//     index = index+2
+// }
+
+let myArray =["AVinash","Rohan","Omkar"]
+
+// let arr = 0;
+// while (arr < myArray.length) {
+
+//     console.log(`value is : ${myArray[arr]}`);
+//     arr++;
+    
+// }
+
+
+
+
+
+// ##### do while loop ####
+
+let score = 11
+
+do {
+    console.log(`Score is: ${score}`);
+    score++;
+} while (score <= 10);
