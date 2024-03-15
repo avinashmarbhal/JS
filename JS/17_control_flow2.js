39d74c6f05895f63e9030740be4e35075e5235e3
Avinash Marbhal <100125775+avinashmarbhal@users.noreply.github.com>
2024-03-15 06:12:45 +0000

    switch  case

---
+++ JS/17_control_flow2.js
@@ -0,0 +1,42 @@
+// const month = 5;
+
+// switch (month) {
+//     case 1:
+//         console.log("Jan");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("mar");
+//         break;              // if we forgot to add break in that case all the code after that is get executed except default
+//     case 4:
+//         console.log("apr");
+//         break;
+
+//     default:
+//         console.log("defualt case");
+//         break;
+// }
+
+
+const month = "a";
+
+switch (month) {
+    case "a":
+        console.log("Jan");
+        break;
+    case "b":
+        console.log("feb");
+        break;
+    case "c":
+        console.log("mar");
+        break;              
+    case "d":
+        console.log("apr");
+        break;
+
+    default:
+        console.log("defualt case");
+        break;
+}
